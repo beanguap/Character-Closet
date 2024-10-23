8c9b653d90d37a58875768372bced22c3dc85a7c
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-10-23 17:07:49 -0400

    Refactor SwipeCard component and add welcome message to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-
+import { useState } from 'react';
+import SwipeCard from '../components/SwipeCard';
+import '../styles/SwipeCard.css';
 
 const Home = () => {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleComplete = () => {
+    setShowWelcome(false);
+  };
+
   return (
     <div>
+      {showWelcome && <SwipeCard onComplete={handleComplete} />}
+      {/* Your existing home page content */}
       <h1>Welcome to Character Closet</h1>
-      <p>Customize outfits and explore fashion!</p>
+      <p>Customize outfits and explore fashion</p>
     </div>
   );
 };
